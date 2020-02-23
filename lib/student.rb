f862be7ab11128def37c97dcd9987cf67e338c15
Chad Lengefeld <chadlengefeld@gmail.com>
2020-02-23 06:33:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,7 @@ class Student
          WHERE name = ?
       SQL
       result =  self.new_from_db
-      DB[:conn].execute(sql, result)
+      DB[:conn].execute(sql)
     end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
